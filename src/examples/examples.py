abc292d38e8109b6745ded15aa16907ead2069fc
Lucas Weinknecht <lucasweinknecht@gmail.com>
2025-08-06 17:35:01 +0200

    Refactor code to work with package structure, fix formatting and add typing hints.

--- src/examples/examples.py
+++ src/examples/examples.py
@@ -1,29 +1,35 @@
+import json
 from texttemplater import replace
 
-if __name__ == '__main__':
-    
-    #  U S A G E   E X A M P L E S
-    
-    template = 'Hello {{name}}, [[you are a {{profession}}||you have no profession]][[, and you are {{adjective}}!||.]]{{nevermatch}}'
+"""Usage examples."""
 
-    data = [
-        {'name': 'Nick', 'profession': 'programmer', 'adjective': 'handsome'},
-        {'name': 'Klaus', 'profession': 'manager'},
-        {'name': 'Peter', 'adjective': 'useless'}
-    ]
+# define template using placeholders
+# name, profession, adjective, nevermatch
+template = """Hello {{name}},
+[[you are a {{profession}}||you have no profession]][[,
+and you are {{adjective}}!||.]]{{nevermatch}}"""
 
-    print(f'Template:\n{template}\n')
-    print('Replaced texts:')
-    [print(replace(template, d)) for d in data]
+# some incomplete datasets
+data = [
+    {"name": "Nick", "profession": "programmer", "adjective": "handsome"},
+    {"name": "Klaus", "profession": "manager"},
+    {"name": "Peter", "adjective": "useless"}
+]
+
+if __name__ == "__main__":
+    print(f"Raw Template:\n{template}\n")
+    print(f"Raw Data:\n{json.dumps(data, indent=2)}\n")
+    print("Replaced texts:")
+    [print(replace(template, d), end="\n\n") for d in data]
 
     # beware empty scopes and else blocks
     print()
     templates = [
-            '[[Beware empty scopes]]',
-            '[[Beware empty scopes {{x}}]]',
-            '[[Beware empty scopes [[{{x}}||xxx]]]]'
+            "[[Beware empty scopes]]",
+            "[[Beware empty scopes {{x}}]]",
+            "[[Beware empty scopes [[{{x}}||xxx]]]]"
     ]
     for template in templates:
-        print(template, '=>', replace(template, {}))
-    template = '[[But if {{y}} then [[{{x}}||xxx]]]]'
-    print(template, '=>', replace(template, {'y': 'y'}))
+        print(template, "=>", replace(template, {}))
+    template = "[[But if {{y}} then [[{{x}}||xxx]]]]"
+    print(template, "=>", replace(template, {"y": "y"}))
--- src/texttemplater/texttemplater.py
+++ src/texttemplater/texttemplater.py
@@ -2,51 +2,77 @@
 Replace placeholder in template text. See help for :replace:
 """
 
-def replace(template, data, _in_scope=False):
-    """
-    Returns text of :template: where all placeholder
-    are replaced with corresponding :data: dict values:
-    :data: is a dict like
-           {'placeholder-name': 'value', ...}.
-    
-    :template: is a string containing text and placeholders like:
-    {{placeholder-name}} will be replaced by 'value'.
-    
+SCOPE_OPEN = "[["
+SCOPE_CLOSE = "]]"
+PLACEHOLDER_OPEN = "{{"
+PLACEHOLDER_CLOSE = "}}"
+ELSE_BLOCK = "||"
+
+
+def replace(
+        template: str,
+        data: dict[str, str],
+        _in_scope: bool = False
+) -> str:
+    """\
+    Return text of :template:, replace all placeholders
+    with corresponding :data: dict values:
+    :data: is a dict like:
+           {"placeholder-name": "value", ...}.
+
+    :template: is a string containing text with placeholders like:
+    "{{placeholder-name}}" will be replaced by "value".
+
     :template: can also contain [[scopes]], which will be deleted
     or replaced by the else block, if any placeholder inside is
-    not a key in :data:.
+    not an existig key in :data:.
     """
     text = template[:]
-    # recursive replace scopes
-    while '[[' in text:
+
+    # recursively replace scopes
+    while SCOPE_OPEN in text:
         scope_start, scope_end = _find_scope(text)
-        text = text[:scope_start] + replace(text[scope_start+2:scope_end-2], data, _in_scope=True) + text[scope_end:]
+        replaced_scope = replace(
+            text[scope_start+2:scope_end-2],
+            data,
+            _in_scope=True
+        )
+        text = text[:scope_start] + replaced_scope + text[scope_end:]
+
     # split else block
-    else_start = _find_before(text, '||', len(text))
-    else_block = '' 
+    else_start = _find_before(text, ELSE_BLOCK, len(text))
+    else_block = ""
     if 0 <= else_start:
         else_block = text[else_start+2:]
         text = text[:else_start]
-    
+
     # replace placeholders
     placeholders = _get_placeholders(text)
-    if _in_scope: # return empty string or else_block if not all placeholders are defined in data
+
+    # return empty string or else_block
+    # if not all placeholders are defined in data
+    if _in_scope:
         if not placeholders or not all([ph in data for ph in placeholders]):
-            return '' if else_start < 0 else replace(else_block, data)
+            return "" if else_start < 0 else replace(else_block, data)
     for ph in placeholders:
-        text = text.replace('{{'+ph+'}}', data.get(ph, ''))
-    else_start = _find_before(text, '||', len(text))
+        placeholder_string = f"{PLACEHOLDER_OPEN}{ph}{PLACEHOLDER_CLOSE}"
+        text = text.replace(placeholder_string, data.get(ph, ""))
+    else_start = _find_before(text, ELSE_BLOCK, len(text))
     if not _in_scope or else_start < 0:
         return text
     return text[:else_start]
 
-def get_placeholders(template):
-    """returns a list of all placeholder names in :template:"""
-    return " ".join(_get_placeholders(template)) \
-              .replace('{{', '').replace('}}', '').split(' ')
 
-def _find_before(text, find_str, before_pos):
-    """returns the position of :find_str: in :text:
+def get_placeholders(template: str) -> str:
+    """return a list of all placeholder names in :template:"""
+    return " ".join(_get_placeholders(template)) \
+              .replace(PLACEHOLDER_OPEN, "")     \
+              .replace(PLACEHOLDER_CLOSE, "")    \
+              .split(" ")
+
+
+def _find_before(text: str, find_str: str, before_pos: int) -> int:
+    """return the position of :find_str: in :text:
     before position :before_pos:"""
     pos = text.find(find_str)
     if before_pos < pos or pos < 0:
@@ -57,47 +83,50 @@ def _find_before(text, find_str, before_pos):
         pos = text.find(find_str, pos+1)
     return prv
 
-def _get_placeholders(text):
-    """returns a list of placeholders in text"""
+
+def _get_placeholders(text: str) -> set[str]:
+    """return set of placeholders in :text:"""
     phs = []
-    for ph in text.split('{{')[1:]:
-        phs.append(ph[:ph.find('}}')])
+    for ph in text.split(PLACEHOLDER_OPEN)[1:]:
+        phs.append(ph[:ph.find(PLACEHOLDER_CLOSE)])
     return set(phs)
 
-def _find_scope(text):
-    """returns position of "[[" and corresponding closing "]]"
+
+def _find_scope(text: str) -> tuple[int, int]:
+    f"""return position of {SCOPE_OPEN} and corresponding closing {SCOPE_CLOSE}
     in :text: as tuple, respecting inner scopes.
-    returns (-1, -1) if there is no scope in :text:"""
-    start = text.find('[[')
-    end = text.find(']]', start)
+    return (-1, -1) if there is no scope in :text:"""
+    start = text.find(SCOPE_OPEN)
+    end = text.find(SCOPE_CLOSE, start)
     if start < 0 or end < 0:
         return -1, -1
-    
-    def next_symbol(text, cursor):
-        """returns (position, symbol) of the next symbol
+
+    # find the true end of the scope
+    def next_symbol(text: str, cursor: int) -> tuple[int, str]:
+        """return (position, symbol) of the next symbol
         in :text: after :cursor:"""
-        next_start = text.find('[[', cursor)
-        next_end = text.find(']]', cursor)
+        next_start = text.find(SCOPE_OPEN, cursor)
+        next_end = text.find(SCOPE_CLOSE, cursor)
         if 0 <= next_start < next_end:
-            return next_start, '[[' 
-        return next_end, ']]'
-    
+            return next_start, SCOPE_OPEN
+        return next_end, SCOPE_CLOSE
+
+    # ignore inner scopes
     next_pos, next_symb = next_symbol(text, start+2)
     inner_scope_counter = 0
-    if next_symb == '[[':
+    if next_symb == SCOPE_OPEN:
         inner_scope_counter += 1
-    else: # didn't find new opening scope symbol:
+    else:
+        # didn't find opening scope symbol:
         return start, next_pos+2
-    
     while 0 < inner_scope_counter:
         next_pos, next_symb = next_symbol(text, next_pos+2)
-        if next_symb == '[[':
+        if next_symb == SCOPE_OPEN:
             inner_scope_counter += 1
         elif next_pos < 0:
             break
-        elif next_symb == ']]':
+        elif next_symb == SCOPE_CLOSE:
             inner_scope_counter -= 1
     # find final closing scope symbol
     next_pos, next_symb = next_symbol(text, next_pos+2)
     return start, next_pos+2
-
